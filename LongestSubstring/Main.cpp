aa696532f5ce8ac57db3261d58d4fbd3d2c9223d
chailyuan <chailyuan2@gmail.com>
2016-11-20 19:21:09 +0800

    我的程序，但是超时了

--- LongestSubstring/Main.cpp
+++ LongestSubstring/Main.cpp
@@ -6,8 +6,37 @@ using namespace std;
 class Solution {
 public:
 	int lengthOfLongestSubstring(string s) {
+		int length = s.length();
+		int max=0;
+		if (length<=1)
+		{
+			return length;
+		}
+		
+		for (int i = 0; i < length;i++)
+		{
+			for (int j = i+1; j < length;j++)
+			{
+				for (int k = i; k < j;k++)
+				{
+					if(s[k] == s[j])
+					{
+						if (j - i >= max)
+						{
+							max = j - i;
+						}
+						j = length + 1;
+						break;
 
-		return 0;
+					}
+					
+				}
+				if(j != length + 1)
+					if (j - i + 1 >= max)
+						max = j - i + 1;
+			}
+		}
+		return max;
 	}
 };
 
@@ -15,7 +44,7 @@ int main()
 {
 	Solution s;
 
-	string str = "fasyefndguasefhdsfue";
+	string str = "abba";
 
 	int r = s.lengthOfLongestSubstring(str);
 }
