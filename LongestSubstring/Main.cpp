1d8a155a0a66290a383069b5bdd59619092c7f54
chailyuan <chailyuan2@gmail.com>
2016-11-20 16:21:40 +0800

    基本框架

--- LongestSubstring/Main.cpp
+++ LongestSubstring/Main.cpp
@@ -1,5 +1,21 @@
+#include <string>
+
+using namespace std;
+
+
+class Solution {
+public:
+	int lengthOfLongestSubstring(string s) {
+
+		return 0;
+	}
+};
 
 int main()
 {
+	Solution s;
 
+	string str = "fasyefndguasefhdsfue";
+
+	int r = s.lengthOfLongestSubstring(str);
 }
