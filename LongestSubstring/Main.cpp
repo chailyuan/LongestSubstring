1a47fcb3770192c23f376b46f960bc77b337fed2
chailyuan-notebook <chailyuan2@gmail.com>
2016-11-21 17:41:16 +0800

    2016-11-21 17:42:19 还没完成用map的实现，回家试试 现在不明白几个map的区别：map，unordere_map，hash_map 回家研究看看

--- LongestSubstring/Main.cpp
+++ LongestSubstring/Main.cpp
@@ -4,24 +4,38 @@
 #include <map>
 
 using namespace std;
-
+/*
+the basic idea is, keep a hashmap which stores the characters in string as keys and their positions as values, and keep two pointers which define the max substring. move the right pointer to scan through the string , and meanwhile update the hashmap. If the character is already in the hashmap, then move the left pointer to the right of the same character last found. Note that the two pointers can only move forward.
+public int lengthOfLongestSubstring(String s) {
+if (s.length()==0) return 0;
+HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+int max=0;
+for (int i=0, j=0; i<s.length(); ++i){
+if (map.containsKey(s.charAt(i))){
+j = Math.max(j,map.get(s.charAt(i))+1);
+}
+map.put(s.charAt(i),i);
+max = Math.max(max,i-j+1);
+}
+return max;
+}
+*/
 
 class Solution {
 public:
 	int lengthOfLongestSubstring(string s) {
 		
-		int ref[256];
-		fill_n(ref, 256, -1);
-		int left = 0;
-		int length = 0;
-		for (int i = 0; i<s.length(); i++) {
-			if (ref[(int)s[i]]+1>left) 
-				left = ref[(int)s[i]] + 1;
-			else if ((i - left)>length - 1) 
-				length = (i - left + 1);
-			ref[(int)s[i]] = i;
+		map<char,int> myMap;
+		int length=0;
+		int i=0,j=0;
+
+		for (;i<s.length();i++)
+		{
+			if (myMap.find(s[i])!=myMap.end())
+			{
+
+			}
 		}
-		return length;
 
 	}
 };
